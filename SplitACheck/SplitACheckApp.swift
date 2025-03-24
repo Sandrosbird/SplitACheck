8c5f3374299ea19591f2e4d32ac8adb7cd87351e
Emil Mescheryakov <emil.sam12@gmail.com>
2025-03-24 23:08:38 +0300

    v1.0 working app with native ios design

---
+++ SplitACheck/SplitACheckApp.swift
@@ -0,0 +1,17 @@
+//
+//  SplitACheckApp.swift
+//  SplitACheck
+//
+//  Created by Emil Mescheryakov on 24.03.2025.
+//
+
+import SwiftUI
+
+@main
+struct SplitACheckApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
